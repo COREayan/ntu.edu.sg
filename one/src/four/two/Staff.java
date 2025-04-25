81292b51ef8a230fd701f55ee537154d99d74829
COREayan <63402298+COREayan@users.noreply.github.com>
2025-04-26 00:25:48 +0530

    update four
    update four

--- one/src/four/two/Staff.java
+++ one/src/four/two/Staff.java
@@ -1,8 +1,14 @@
 package four.two;
 
-public class Staff {
+public class Staff extends Person{
     private String school;
     private double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
 
     public String getSchool() {
         return school;
